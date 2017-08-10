d2f4036df655bd066606a486da9d5ea447fd760e
Dmitry Ledentsov <dmlled@yahoo.com>
2017-08-10 22:50:42 +0200

    v17.8 provisioned externally

--- provisioned_externally/setup_pipelines.py
+++ provisioned_externally/setup_pipelines.py
@@ -11,14 +11,18 @@ url_ssl = "https://go-server:8154"
 config_url = url_ssl + "/go/api/admin/config.xml"
 
 print("Waiting for " + url)
+sys.stdout.flush()
 if not wait_for_url(url, 300):
     print("Go server did not start in a timely fashion. Please retry docker-compose up provisioner")
     sys.exit()
 
 print("Go server is up...")
+sys.stdout.flush()
 
 print("Sleeping a bit...")
-time.sleep(100)
+sys.stdout.flush()
+
+time.sleep(30)
 
 orig = requests.api.request('get', config_url, verify=False)
 md5 = orig.headers['x-cruise-config-md5']
@@ -30,15 +34,15 @@ xml_old = re.sub(r'(?is)<pipelines group.+/>', '', xml_old)
 xml_old = re.sub(r'(?is)agentAutoRegisterKey=\".*?\"', 'agentAutoRegisterKey=\"123456789abcdef\"', xml_old)
 
 repos = """<config-repos>
-  <config-repo plugin="yaml.config.plugin">
-    <git url="https://github.com/d-led/gocd-rpi-unicorn-hat-monitor.git" />
-  </config-repo>
-  <config-repo plugin="yaml.config.plugin">
-    <git url="https://github.com/d-led/automatic-lua-property-tables.git" />
-  </config-repo>
-  <config-repo plugin="yaml.config.plugin">
-    <git url="https://github.com/d-led/dont_wait_forever_for_the_tests.git" />
-  </config-repo>
+    <config-repo plugin="yaml.config.plugin" id="gocd-rpi-unicorn-hat-monitor">
+      <git url="https://github.com/d-led/gocd-rpi-unicorn-hat-monitor.git" />
+    </config-repo>
+    <config-repo plugin="yaml.config.plugin" id="automatic-lua-property-tables">
+      <git url="https://github.com/d-led/automatic-lua-property-tables.git" />
+    </config-repo>
+    <config-repo plugin="yaml.config.plugin" id="dont_wait_forever_for_the_tests">
+      <git url="https://github.com/d-led/dont_wait_forever_for_the_tests.git" />
+    </config-repo>
 </config-repos>
 <pipelines group="defaultGroup" />
 """
